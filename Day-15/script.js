b75b5e14b8a675304c234d74dd3bc16dbcc6420e
renuperumalla <renuperumalla@gmail.com>
2026-02-11 22:50:12 +0530

    🚀 Day 15 - index.html - 10:50:04 pm

---
+++ Day-15/script.js
@@ -0,0 +1,7 @@
+// Day 15 Logic Practice
+console.log('Lab Session 15 Started');
+
+const display = document.getElementById('display');
+display.innerText = 'Logic Engine Online';
+
+// Practice your JS code here...
